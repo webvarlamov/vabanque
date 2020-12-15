e35a072d1bdea3808eb8c98b1f4016b52c4348d3
varlamov <varlamov@nemosoft.ru>
2020-12-15 17:53:19 +0300

    Issue #2. Added REST-API layer

--- modules/web/test/com/company/vabanque/VabanqueWebTestContainer.java
+++ modules/web/test/com/company/vabanque/VabanqueWebTestContainer.java
@@ -8,10 +8,9 @@ import java.util.Arrays;
 public class VabanqueWebTestContainer extends TestContainer {
 
     public VabanqueWebTestContainer() {
-        appComponents = new ArrayList<>(Arrays.asList(
-                "com.haulmont.cuba"
-                // add CUBA add-ons and custom app components here
-        ));
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.addon.restapi");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the web module
